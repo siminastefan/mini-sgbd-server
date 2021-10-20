e58291f7e75e1b34171b5667303295a6aeec3891
siminastefan <61596690+siminastefan@users.noreply.github.com>
2021-10-20 17:19:50 +0300

    Update TableService.java

--- src/main/java/com/ubb/minisgbdserver/service/TableService.java
+++ src/main/java/com/ubb/minisgbdserver/service/TableService.java
@@ -35,6 +35,7 @@ public class TableService {
         Document document = documentBuilder.parse(FILENAME);
 
         NodeList databases = document.getElementsByTagName("Database");
+
         for (int index = 0; index < databases.getLength(); index++) {
             String databaseName = databases.item(index).getAttributes().getNamedItem("databaseName").getNodeValue();
             if (databaseName.equals(tableDTO.getDatabaseName())) {
@@ -47,26 +48,33 @@ public class TableService {
                 Element newStructure = document.createElement("Structure");
                 Element newPrimaryKey = document.createElement("PrimaryKey");
                 List<String> fields = Arrays.asList(tableDTO.getFields().split(","));
-                String pkColumns = tableDTO.getFields().substring(tableDTO.getFields().indexOf("primary key"));
-                if (pkColumns.contains("primary key")) {
-                    String pks = pkColumns.substring(pkColumns.indexOf("(") + 1, pkColumns.lastIndexOf(")"));
-                    for (String col : pks.split(",")) {
-                        Element newPrimaryKeyAttribute = document.createElement("pkAttribute");
-                        newPrimaryKeyAttribute.setTextContent(col);
-                        newPrimaryKey.appendChild(newPrimaryKeyAttribute);
-                    }
-                }
-                newTable.appendChild(newPrimaryKey);
-                for (int j = 0; j < fields.size() - 2; j ++) {
+                Element newForeignKeys = document.createElement("ForeignKeys");
+
+                for (String field : fields) {
                     //TODO: check for unique string to add unique attributes in xml
-                    String name = fields.get(j).split(" ")[0];
-                    String type = fields.get(j).split(" ")[1];
+                    //TODO: check for not null string
+                    String name = field.split(" ")[0];
+                    String type = field.split(" ")[1];
+                    if (field.contains("primary key")) {
+                        createPrimaryKey(document, newPrimaryKey, name);
+                    } else if (field.contains("references")) {
+                        //GroupId int REFERENCES groups (GroupID)
+                        Element newForeignKey = createForeignKey(document, field, name);
+                        newForeignKeys.appendChild(newForeignKey);
+                    }
                     Element newAttribute = document.createElement("Attribute");
+                    if (type.contains("char")) {
+                        newAttribute.setAttribute("type", "char");
+                        newAttribute.setAttribute("length", type.substring(type.indexOf("(") + 1, type.lastIndexOf(")")));
+                    } else {
+                        newAttribute.setAttribute("type", type);
+                    }
                     newAttribute.setAttribute("attributeName", name);
-                    newAttribute.setAttribute("type", type);
                     newStructure.appendChild(newAttribute);
                 }
+                newTable.appendChild(newPrimaryKey);
                 newTable.appendChild(newStructure);
+                newTable.appendChild(newForeignKeys);
                 databases.item(index).appendChild(newTable);
 
                 DOMSource source = new DOMSource(document);
@@ -84,11 +92,33 @@ public class TableService {
         return false;
     }
 
+    private void createPrimaryKey(Document document, Element newPrimaryKey, String name) {
+        Element newPrimaryKeyAttribute = document.createElement("pkAttribute");
+        newPrimaryKeyAttribute.setTextContent(name);
+        newPrimaryKey.appendChild(newPrimaryKeyAttribute);
+    }
+
+    private Element createForeignKey(Document document, String field, String name) {
+        String table = field.split(" ")[3];
+        String col = field.substring(field.indexOf("(") + 1, field.lastIndexOf(")"));
+        Element newForeignKey = document.createElement("ForeignKey");
+        Element newFqAttribute = document.createElement("fqAttribute");
+        newFqAttribute.setTextContent(name);
+        Element references = document.createElement("references");
+        Element refTable = document.createElement("refTable");
+        refTable.setTextContent(table);
+        Element refAttribute = document.createElement("refAttribute");
+        refAttribute.setTextContent(col);
+        references.appendChild(refTable);
+        references.appendChild(refAttribute);
+        newForeignKey.appendChild(newFqAttribute);
+        newForeignKey.appendChild(references);
+        return newForeignKey;
+    }
+
     public Node findTable(String databaseName, String tableName) throws Exception {
         Node database = databaseService.findDatabase(databaseName);
         for (int i = 0; i < database.getChildNodes().getLength(); i++) {
-            System.out.println(database.getAttributes().getNamedItem("databaseName").getNodeValue());
-            System.out.println(database.getChildNodes().getLength());
             if (database.getChildNodes().item(i).getNodeType() == Node.ELEMENT_NODE) {
                 if (database.getChildNodes().item(i).getAttributes().getNamedItem("tableName").getNodeValue().equals(tableName))
                     return database.getChildNodes().item(i);
